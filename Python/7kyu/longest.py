0a8ff6245f3729703a78f52438dabb27bca2b033
ej2kd <eiji.tsukada9415@gmail.com>
2023-01-04 23:46:25 +0900

    add 7kyu Python

---
+++ Python/7kyu/longest.py
@@ -0,0 +1,13 @@
+def longest(a1, a2):
+    """ 
+    Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+
+    Examples:
+    a = "xyaabbbccccdefww"
+    b = "xxxxyyyyabklmopq"
+    longest(a, b) -> "abcdefklmopqwxy"
+
+    a = "abcdefghijklmnopqrstuvwxyz"
+    longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+    """
+    return ''.join(sorted(list(set(a1).union(set(a2)))))

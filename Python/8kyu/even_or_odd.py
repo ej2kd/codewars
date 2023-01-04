b1ed864f09d1e8461ff5c1e25ea71fe25a967a1d
ej2kd <eiji.tsukada9415@gmail.com>
2023-01-05 00:04:52 +0900

    add 8kyu python

---
+++ Python/8kyu/even_or_odd.py
@@ -0,0 +1,5 @@
+def even_or_odd(number):
+    """
+    Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+    """
+    return "Odd" if number % 2 else "Even"
